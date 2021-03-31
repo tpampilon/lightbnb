a8571f5f5d3492d343e3305fd497bc5c7277ea6c
TedP <ted.pampilon@gmail.com>
2021-03-31 20:19:07 +0000

    add 02_seeds.sql

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,10 +8,10 @@ VALUES (1, 1, 'Casa', 'desciption', 'url', 'url', 100, 2, 3, 4, 'Canada', 'Maple
 (2, 2, 'BigHouse', 'desciption', 'url', 'url', 145, 4, 6, 7, 'Canada', 'Facy Street', 'Montreal','2t3 a6b', true),
 (3, 1, 'Casa Dos', 'desciption', 'url', 'url', 100, 1, 2, 3, 'Canada', 'Syrup', 'Quebec','1t3 a6d', true)
 
-INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
-VALUES (1, 1, '2018-09-11', '2018-09-26'),
-(2, 2, '2019-01-04', '2019-02-01'),
-(3, 3, '2021-10-01', '2021-10-14');
+INSERT INTO reservations(id, guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, 3, '2021-10-01', '2021-10-14');
 
 INSERT INTO property_reviews(id, guest_id, property_id, reservation_id, rating, message)
 VALUES (1, 3, 1, 2, 5, 'message'),
