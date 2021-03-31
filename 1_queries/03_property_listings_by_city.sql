a7b40df619487a981f3f93737cd838ba97ab60cd
TedP <ted.pampilon@gmail.com>
2021-03-31 20:49:41 +0000

    add 03_property_listings_by_city.sql

---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,10 @@
+-- 03_property_listings_by_city
+
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
